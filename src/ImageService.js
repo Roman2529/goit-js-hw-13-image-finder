5a4553a2867f1df0896076f59553f0087adabb31
Roman Demikhov <demikhovroman@gmail.com>
2020-08-19 14:08:02 +0300

    1

--- src/ImageService.js
+++ src/ImageService.js
@@ -1,1 +1,15 @@
 import tamplate from './tamplate.hbs';
+import refs from './refs';
+import { template } from 'handlebars';
+
+export default {
+    draw(data) {
+        const obj = { arr: data };
+        const createList = obj => template(obj);
+        const listPhoto = createList(obj);
+        refs.galleryList.insertAdjacentHTML('beforeend', listPhoto);
+    },
+    clean() {
+        refs.galleryList.innerHTML = '';
+    }
+};
