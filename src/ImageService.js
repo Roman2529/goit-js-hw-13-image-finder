1882ec8d377085e73f6df378cb1e1a3e571dbc90
Roman Demikhov <demikhovroman@gmail.com>
2020-08-21 08:00:10 +0300

    fix some function

--- src/ImageService.js
+++ src/ImageService.js
@@ -2,47 +2,47 @@ import servise from './apiService';
 import tamplate from './tamplate.hbs';
 import refs from './refs';
 
-
 refs.form.addEventListener('submit', imageSearchInputHandler);
 refs.loadMoreButton.addEventListener('click', loadMoreButtonHandler);
 
-
 function imageSearchInputHandler(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const form = e.currentTarget;
-    const input = form.elements.query;
+  const form = e.currentTarget;
+  const input = form.elements.query;
 
-    clearListItems();
+  clearListItems();
 
-    servise.resetPage();
-    servise.searchQuerry = input.value;
+  servise.resetPage();
+  servise.searchQuerry = input.value;
 
-    servise.fetchArticles().then(hits => {
-        const markup = buildListItemsTemplate(hits);
-        userItems(markup);
-    });
-    input.value = '';
+  servise.fetchArticles().then(hits => {
+    const markup = buildListItemsTemplate(hits);
+    userItems(markup);
+  });
+  input.value = '';
+}
+
+function scrollPage() {
+  window.scrollTo({
+    top: 1000,
+    behavior: 'smooth',
+  });
 };
 
 function loadMoreButtonHandler() {
-    servise.fetchArticles().then(hits => {
-        const markup = buildListItemsTemplate(hits);
-        userItems(markup);
-        window.scrollTo(0, 1000);
-
-        window.scrollTo({
-            top: 1000,
-            behavior: 'smooth'
-        });
-    });
-};
+  servise.fetchArticles().then(hits => {
+    const markup = buildListItemsTemplate(hits);
+    userItems(markup);
+    scrollPage();
+  });
+}
 function userItems(items) {
-    refs.galleryList.insertAdjacentHTML('beforeend', items);
-};
+  refs.galleryList.insertAdjacentHTML('beforeend', items);
+}
 function buildListItemsTemplate(items) {
-    return tamplate(items);
-};
+  return tamplate(items);
+}
 function clearListItems() {
-    refs.galleryList.innerHTML = '';
-};
+  refs.galleryList.innerHTML = '';
+}
